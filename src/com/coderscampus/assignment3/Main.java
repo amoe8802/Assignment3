ce4c727f699c1a302dde51d8abbe6cb53ff7d4c8
Abby Moe <161901430+amoe8802@users.noreply.github.com>
2024-05-14 14:47:43 -0400

    2nd commit

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -3,8 +3,8 @@ package com.coderscampus.assignment3;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Plese enter your username and password: ");
+		
 	}
-
 }
