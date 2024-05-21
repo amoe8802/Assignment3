3018aba3029c2708f437819f3894f652fc278343
Abby Moe <161901430+amoe8802@users.noreply.github.com>
2024-05-21 15:47:01 -0400

    7th commit

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -1,17 +1,12 @@
 package com.coderscampus.assignment3;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Plese enter your username and password: ");
-		System.out.println("Username:");
-		String input = scanner.nextLine();
-		System.out.println("Password:");
+		UserService userService = new UserService();
+
+		userService.userLogin();
 		
 	}
 }
